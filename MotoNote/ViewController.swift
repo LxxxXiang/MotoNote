ab7e5c7679f20c2fff7b2bf8114442bff4677d13
lionel <maid1214@gmail.com>
2021-11-20 15:20:50 +0800

    Initial Commit

---
+++ MotoNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotoNote
+//
+//  Created by 廖翔 on 2021/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
